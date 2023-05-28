c8604ba2e6ebc013ea073bdf2e6d912f217042b1
somkakade <somanathkakade5@gmail.com>
2023-05-28 12:04:32 +0530

    added initel level of the index and css file

---
+++ app.js
@@ -0,0 +1,24 @@
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "child1",
+  },
+  "Wait, I am checking"
+);
+
+const heading2 = React.createElement(
+  "h1",
+  { id: "child2" },
+  "I am learning React From Akshy Saini"
+);
+
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  [heading1, heading2]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
